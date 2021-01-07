2930a2af92cbe5f3c5fd8bf640ff107ebf600055
Cociuba Nicusor <cociubanicusor66@gmail.com>
2021-01-07 21:29:46 +0200

    ty

---
+++ Pool2.2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Pool2._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Se da o secventa de n numere. Sa se determina cate sunt negative, cate sunt zero si cate sunt pozitive. 
+            int n, i, a = 0, b = 0, c = 0;
+            Console.WriteLine("Introduceti lungimea secventei:");
+            n = int.Parse(Console.ReadLine());
+            for(i=1;i<=n;i++)
+            {
+                if (i < 0)
+                {
+                    a++;
+                }
+                if(i==0)
+                {
+                    b++;
+                }
+                if (i > 0)
+                    c++;
+            }
+            Console.WriteLine($"In secventa sunt {a} numere negative.");
+            Console.WriteLine($"In secventa sunt {b} numere zero.");
+            Console.WriteLine($"In secventa sunt {c} numere pozitive.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Pool2.3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Pool2._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Calculati suma si produsul numerelor de la 1 la n.
+            int n, i, s = 0, p = 1;
+            Console.WriteLine("Introducet numarul n:");
+            n = int.Parse(Console.ReadLine());
+            for(i=1;i<=n;i++)
+            {
+                s = s + i;
+                p = p * i;
+            }
+            Console.WriteLine($"Suma numerelor pana la n este {s}.");
+            Console.WriteLine($"Produsul numerelor pana la n este {p}.");
+            Console.ReadKey();
+        }
+    }
+}
