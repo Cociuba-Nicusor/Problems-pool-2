f199486ff56f186be28497baedc604a825dc6b60
Cociuba Nicusor <cociubanicusor66@gmail.com>
2021-01-07 20:55:01 +0200

    ty

---
+++ Pool2.1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Pool2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Se da o secventa de n numere. Sa se determine cate din ele sunt pare.
+            int n, i, s = 0;
+            Console.WriteLine("Introduceti numarul n:");
+            n = int.Parse(Console.ReadLine());
+            for (i = 1; i <= n; i++)
+            {
+                if (i % 2 == 0)
+                    s = s + 1;
+            }
+            Console.WriteLine($"In secvemta sunt {s} numere pare.");
+            Console.ReadKey();
+
+        }
+    }
+}
