5c863f649640f777e0effe9296b2e2ee0c29e2df
Cociuba Nicusor <cociubanicusor66@gmail.com>
2021-01-08 19:20:33 +0200

    ty

---
+++ Pool2.8/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Pool2._8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Determianti al n-lea numar din sirul lui Fibonacci. 
+            //Sirul lui Fibonacci se construieste astfel: f1 = 0, f2 = 1, f_n = f_(n-1) + f(n-2). 
+            //Exemplu: 0, 1, 1, 2, 3, 5, 8 ...
+            int n;
+            Console.Write("Introduceti numarul n: ");
+            n = int.Parse(Console.ReadLine());
+            Console.Write($"f({n})  din sir  este: ");
+            Console.Write(Fibonacci(n));
+            Console.WriteLine();
+            Console.ReadKey();
+
+        }
+
+        static int Fibonacci(int n)
+        {
+            if (n <= 1)
+                return n;
+            else
+                return Fibonacci(n - 1) + Fibonacci(n - 2);
+        }
+    }   
+}
