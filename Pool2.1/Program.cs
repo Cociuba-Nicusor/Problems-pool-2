1816527151088a44e01bc2ebe0403afbd0bafe7a
Cociuba Nicusor <cociubanicusor66@gmail.com>
2021-01-08 17:08:55 +0200

    ty

--- Pool2.1/Program.cs
+++ Pool2.1/Program.cs
@@ -4,15 +4,20 @@ namespace Pool2._1
 {
     class Program
     {
+        static Random rnd = new Random();
         static void Main(string[] args)
         {
             //Se da o secventa de n numere. Sa se determine cate din ele sunt pare.
             int n, i, s = 0;
             Console.WriteLine("Introduceti numarul n:");
             n = int.Parse(Console.ReadLine());
+            Console.WriteLine("Secventa generata este:");
             for (i = 1; i <= n; i++)
             {
-                if (i % 2 == 0)
+                int x = rnd.Next(51);
+                
+                Console.WriteLine($"{x}  ");
+                if (x % 2 == 0)
                     s = s + 1;
             }
             Console.WriteLine($"In secvemta sunt {s} numere pare.");
